8950a7729299c89f15f2614802cd71fc8f7af1f2
Quang Nguyen <quang.nguyenln@berkeley.edu>
2022-05-25 08:40:12 +0700

    merging

--- App.js
+++ App.js
@@ -1,24 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { ClosetBody } from './components/ClosetBody.js';
-import { Header } from './components/Header.js';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './navigation/TabNavigator';
 
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
-    <>
-      <Header/>
-      <ClosetBody/>
-    </>
+    <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen component={Intro} name="Intro" />
+        <Stack.Screen component={Home} name="Home" />
+      </Stack.Navigator> */}
+        <TabNavigator />
+    </NavigationContainer>
   );
+}
+
+const Intro = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>fixfit</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.startButton}
+        >
+          <Text>Let's get started!</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+        <Text>Home</Text>
+        <StatusBar style="auto" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex : 1,
-    backgroundColor: '#ffff00',
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      marginBottom: 15,
+  },
+  startButton: {
+    backgroundColor: '#ffff00',
+    borderRadius: 15,
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '6%',
+    marginBottom: 25,
+  }
 });
+
+export default App;

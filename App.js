c4f63fb651053d2cc257675a8ed286b2526491bd
Quang Nguyen <quang.nguyenln@berkeley.edu>
2022-05-25 08:56:54 +0700

    finish header, bottom, and closet

--- App.js
+++ App.js
@@ -4,16 +4,19 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './navigation/TabNavigator';
+import { Header } from './navigation/Header';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
+  
     <NavigationContainer>
       {/* <Stack.Navigator>
         <Stack.Screen component={Intro} name="Intro" />
         <Stack.Screen component={Home} name="Home" />
       </Stack.Navigator> */}
+        <Header />
         <TabNavigator />
     </NavigationContainer>
   );
